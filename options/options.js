0846cec2138c4ae79780fe8202d20e581459d5b0
Crowdbotics <team@crowdbotics.com>
2021-09-19 20:18:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "autumn_resonance_30685Identifier",
+  url: "https://autumn_resonance_30685Identifier.botics.co",
+  api: "https://autumn_resonance_30685Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
